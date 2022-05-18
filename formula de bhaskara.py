f0a2103d48164efd43511f95ae05e879867e045a
cmassiral <104464515+cmassiral@users.noreply.github.com>
2022-05-18 17:18:39 -0300

    Add files via upload

---
+++ formula de bhaskara.py
@@ -0,0 +1,22 @@
+
+a = 1
+b = 2
+c = 1
+
+delta = (b**2)-4*a*c
+
+x1 = (-b + (delta**0.5))/(2*a)
+x2 = (-b - (delta**0.5))/(2*a)
+
+print("delta: {}, x1: {},x2:{}".format(delta, x1, x2))
+
+#a raiz quadrada é igual a potencia de 1/2 ou elevado a 0,5
+#calculo de raiz quadrada
+# x = 100
+# raiz = x **0.5
+#print(raiz)
+
+
+
+
+
